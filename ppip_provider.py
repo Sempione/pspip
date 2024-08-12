3f39630d2a63c834a3ceed40a5f3f628f3c71548
Christian Lesem <96990503+Sempione@users.noreply.github.com>
2024-08-12 14:09:10 +0200

    Expand on last commit

--- ppip_provider.py
+++ ppip_provider.py
@@ -73,7 +73,7 @@ class PutPointsInPolygonsProvider(QgsProcessingProvider):
 
         This string should be short (e.g. "Lastools") and localised.
         """
-        return self.tr('Vector analysis')
+        return self.tr('User-created')
 
     def icon(self):
         """
